4a713bd09c32c5535398264c6bc0b635f6a3ebc5
Ak-yassin <ak.yassin@hotmail.com>
2021-07-23 07:04:04 +0100

    feat: layout and src setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
-// import { AppProps } from "next/app";
-// import Head from "next/head";
+import { AppProps } from "next/app";
+import Head from "next/head";
 // import { ApolloProvider } from "@apollo/client";
 // import { useApollo } from "src/apollo";
 // import { AuthProvider } from "src/auth/useAuth";
-// import "../styles/index.css";
+import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return<>
+  <Head>
+    <title>Browse Local Homes</title>
+    <link rel="icon" href="/favicon.ico"/>
+  </Head>
+   <Component {...pageProps} />
+   </>
 }
---
+++ src/components/layout.tsx
@@ -0,0 +1,46 @@
+import { FunctionComponent, ReactNode } from "react";
+import Link from "next/link";
+// import {useAuth} from 'src/auth/useAuth'
+
+interface IProps {
+  main: ReactNode;
+}
+
+const Layout: FunctionComponent<IProps> = ({ main }) => {
+  const authenticated = false;
+  const logout = () => null;
+
+  return (
+    <div className="bg-gray-900 max-w-screen-3xl mx-auto text-white">
+      <nav className="bg-gray-800" style={{ height: "70px" }}>
+        <div className="px-6 flex items-center justify-between h-16">
+          <Link href="/">
+            <a>
+              <img
+                src="/home-color.svg"
+                alt="home house"
+                className="inline w-6"
+              />
+            </a>
+          </Link>
+
+          {authenticated ? (
+            <>
+              <Link href="/houses/add">
+                <a>Add House</a>
+              </Link>
+              <button onClick={logout}>Logout</button>
+            </>
+          ) : (
+            <Link href="/auth">
+              <a className=" font-medium">Sign Up</a>
+            </Link>
+          )}
+        </div>
+      </nav>
+      <main className="bg-red-900" style={{minHeight: "calc(100vh - 70px)"}}>{main}</main>
+    </div>
+  );
+};
+
+export default Layout;
