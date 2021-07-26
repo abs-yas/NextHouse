b4be5c37585ea63bd887d4196d10788d1a405db9
Ak-yassin <ak.yassin@hotmail.com>
2021-07-26 06:22:23 +0100

    feat: quick query test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-// import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
 // import { useDebounce } from "use-debounce";
 import Layout from "src/components/layout";
 import Map from "src/components/map";
@@ -8,7 +8,15 @@ import Map from "src/components/map";
 // import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery, HousesQueryVariables } from "src/generated/HousesQuery";
 
+const HELLO_QUERY = gql`
+  query HelloQuery {
+    hello
+  }
+`;
+
 export default function Home() {
+  const { data, loading } = useQuery(HELLO_QUERY);
+
   return (
     <Layout
       main={
@@ -18,8 +26,9 @@ export default function Home() {
             style={{ maxHeight: "calc(100vh - 70px)", overflowX: "auto" }}
           >
             HouseList
+            <pre>{JSON.stringify(data, null, 2)}</pre>
           </div>
-          <div className="w-1/2"> 
+          <div className="w-1/2">
             <Map />
           </div>
         </div>
